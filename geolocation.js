f921096c5a4f5842d8ea7953bf4ba627e5bf399e
Perfectrum <38108124+Perfectrum@users.noreply.github.com>
2018-05-18 03:53:27 +0700

    Add files via upload

---
+++ geolocation.js
@@ -0,0 +1,42 @@
+﻿ymaps.ready(init);
+
+function init() {
+
+    var geolocation = ymaps.geolocation,
+
+        myMap = new ymaps.Map('map', {
+
+            center: [53.355084, 83.769948],
+
+            zoom: 10
+
+        }, {
+
+            searchControlProvider: 'yandex#search'
+
+        });
+
+
+    // Сравним положение, вычисленное по ip пользователя и
+
+    // положение, вычисленное средствами браузера.
+
+    geolocation.get({
+
+        provider: 'browser',
+
+        mapStateAutoApply: true
+
+    }).then(function (result) {
+
+        // Синим цветом пометим положение, полученное через браузер.
+
+        // Если браузер не поддерживает эту функциональность, метка не будет добавлена на карту.
+
+        result.geoObjects.options.set('preset', 'islands#blueCircleIcon')
+
+        myMap.geoObjects.add(result.geoObjects);
+
+    });
+
+}
