22f109f1345a37de3dff088b9259506faa4a9aa8
Perfectrum <38108124+Perfectrum@users.noreply.github.com>
2018-05-18 06:12:35 +0700

    Add files via upload

--- js/geolocation.js
+++ js/geolocation.js
@@ -23,29 +23,6 @@ function init() {
 
     geolocation.get({
 
-        provider: 'yandex',
-
-        mapStateAutoApply: true
-
-    }).then(function (result) {
-
-        // Красным цветом пометим положение, вычисленное через ip.
-
-        result.geoObjects.options.set('preset', 'islands#redCircleIcon');
-
-        result.geoObjects.get(0).properties.set({
-
-            balloonContentBody: 'Мое местоположение'
-
-        });
-
-        myMap.geoObjects.add(result.geoObjects);
-
-    });
-
-
-    geolocation.get({
-
         provider: 'browser',
 
         mapStateAutoApply: true
@@ -62,4 +39,38 @@ function init() {
 
     });
 
+
+    objectManager = new ymaps.ObjectManager({
+
+            // Чтобы метки начали кластеризоваться, выставляем опцию.
+
+            clusterize: true,
+
+            // ObjectManager принимает те же опции, что и кластеризатор.
+
+            gridSize: 32,
+
+            clusterDisableClickZoom: true
+
+        });
+
+    // Чтобы задать опции одиночным объектам и кластерам,
+
+    // обратимся к дочерним коллекциям ObjectManager.
+
+    objectManager.objects.options.set('preset', 'islands#greenDotIcon');
+    objectManager.clusters.options.set('preset', 'islands#greenClusterIcons');
+
+    myMap.geoObjects.add(objectManager);
+
+
+    $.ajax({
+
+        url: "data.json"
+
+    }).done(function(data) {
+
+        objectManager.add(data);
+
+    });
 }
